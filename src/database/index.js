ab1e655e16a5c4f65bb424c8581d1c03617248df
Leonardo Antunes <leoantunesnunes@gmail.com>
2020-01-30 08:46:14 -0300

    final

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://leooantunes:leo123456@api-db-ulrwt.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
